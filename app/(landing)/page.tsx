07aacc7942d7dd37c87d6862d39d073914e4c5a5
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-03-09 14:58:01 +0000

    Move root page to the route group

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+    const router = useRouter();
+
+    return <>
+        <Button onClick={ () => router.push("/fridge") }>Go to app!</Button>
+    </>;
+}
