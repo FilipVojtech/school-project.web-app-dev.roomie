87efabc5758d4bd85daf919f5ccf6be863976517
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-03-12 14:02:43 +0000

    Move page header outside layout. Pages will have to implement them themselves

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,14 +1,9 @@
-"use client";
-
-import Header from "@/app/(app)/Header";
+import React from "react";
 import Navigation from "@/app/(app)/Navigation";
-import React, { useState } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    const [ pageTitle, changeTitle ] = useState("Roomie app");
-
     return <div className="flex flex-col h-full md:flex-row">
-        <div id="content" className="flex flex-col h-full lg:w-1/2 lg:m-auto">
+        <div id="content" className="flex flex-col h-full w-full rounded-md lg:w-1/2 lg:m-auto">
             { children }
         </div>
         <Navigation/>
