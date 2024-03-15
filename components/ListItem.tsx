1028ed7fe00fd2f60e284c02c34b009a8eb1d390
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-03-15 15:58:16 +0000

    Indicate interactive items

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { clsx } from "clsx";
 
-export default function ListItem({ className, children }: { className?: string, children: React.ReactNode }) {
-    return <div className={ clsx("rounded-md bg-blue-100 p-2", className) }>
+export default function ListItem({ className, children, interactive = true }: {
+    className?: string,
+    children: React.ReactNode,
+    interactive?: boolean
+}) {
+    return <div className={ clsx("rounded-md bg-blue-100 p-2 h-min", className, { "interactive": interactive }) }>
         { children }
     </div>;
 }
