7c6f5653ce9ebd34d08a7deb90dab58957f83726
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-03-15 17:02:59 +0000

    Specify the type for useState

--- app/(app)/calendar/CalendarView.tsx
+++ app/(app)/calendar/CalendarView.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import Calendar from 'react-lightweight-calendar';
 
 export default function CalendarView() {
-    const [ currentDate, setCurrentDate ] = React.useState('2024-03-15');
+    const [ currentDate, setCurrentDate ] = React.useState<Date | string>('2024-03-15');
 
     return <>
         <Calendar
