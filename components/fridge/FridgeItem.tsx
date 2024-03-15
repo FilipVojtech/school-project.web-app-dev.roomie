ad6d0479b89fd3f9b826095bf81035dc57cfd918
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-03-15 12:04:19 +0000

    Implement Fridge Item

--- components/fridge/FridgeItem.tsx
+++ components/fridge/FridgeItem.tsx
@@ -2,28 +2,32 @@
 
 import ListItem from "@/components/ListItem";
 import { Clock, Minus, Plus, UsersRound } from "lucide-react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
-export default function FridgeItem({ name = "Hello", bestBefore, owners }: {
+export default function FridgeItem({ id, name, bestBefore, owners }: {
+    id: string;
     name: string;
     bestBefore?: Date;
     owners: { [key: symbol]: any, firstName: string, lastName: string }[];
 }) {
     const [ quantity, setQuantity ] = useState(1);
-    let ownerString: string = "You";
+    let ownersString: string = "You";
 
     /**
      * Handle change while ensuring the value does not decrease below 0
      * @param value
+     * @param event
      */
-    function handleChange(value: string | number) {
+    function handleChange(value: string | number, event: React.MouseEvent<HTMLButtonElement> | React.ChangeEvent) {
         const newValue: number = Math.max(
             0,
             typeof value == "string" ? Number.parseInt(value) : value
         );
 
+        preventEventPropagation(event);
         setQuantity(newValue);
     }
 
@@ -33,53 +37,64 @@ export default function FridgeItem({ name = "Hello", bestBefore, owners }: {
         }
     }
 
+    function preventEventPropagation(event: any) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
     if (owners.length > 1) {
-        for (const owner of owners) {
-            ownerString += `, ${ owner.firstName } ${ owner.lastName.charAt(0) }`;
+        for (let i = 1; i < owners.length; i++) {
+            const { firstName, lastName } = owners[i];
+            ownersString += `, ${ firstName } ${ lastName.charAt(0) }.`;
         }
     }
 
-    return <ListItem className="flex justify-between">
-        <div className="flex flex-col gap-0.5">
-            <div className="text-2xl">{ name }</div>
-            {
-                bestBefore &&
-                <div className="flex items-center gap-1">
-                    <Clock className="h-5 w-5"/>
-                    { bestBefore.toDateString() }
+    return <Link href={ `/fridge/${ id }/edit` }>
+        <ListItem className="flex justify-between gap-1 h-min">
+            <div className="flex-1 flex-col gap-0.5">
+                <div className="text-2xl">{ name }</div>
+                {
+                    bestBefore &&
+                    <div className="flex gap-1">
+                        <Clock className="h-5 w-5"/>
+                        { bestBefore.toDateString() }
+                    </div>
+                }
+                <div className="flex gap-1">
+                    <div>
+                        <UsersRound className="h-5 w-5"/>
+                    </div>
+                    <span className="max-w-fit">{ ownersString }</span>
                 </div>
-            }
-            <div className="flex items-center gap-1">
-                <UsersRound className="h-5 w-5"/>
-                { ownerString }
             </div>
-        </div>
-        <div className="flex items-end">
-            <div className="flex rounded-md overflow-clip h-fit">
-                <Button
-                    variant="outline"
-                    size="icon"
-                    className="border-0 rounded-none"
-                    onClick={ () => handleChange(quantity + 1) }
-                >
-                    <Plus size={ 16 }/>
-                </Button>
-                <Input
-                    type="number"
-                    value={ quantity }
-                    className="w-14 text-lg text-center border-0 border-x-2 rounded-none no-spin-buttons"
-                    onChange={ e => handleChange(e.target.value) }
-                    onBlur={ e => handleBlur(e.target.value) }
-                />
-                <Button
-                    variant="outline"
-                    size="icon"
-                    className="border-0 rounded-none"
-                    onClick={ () => handleChange(quantity - 1) }
-                >
-                    <Minus size={ 16 }/>
-                </Button>
+            <div className="flex items-end">
+                <div className="flex rounded-md overflow-clip h-fit">
+                    <Button
+                        variant="outline"
+                        size="icon"
+                        className="border-0 rounded-none"
+                        onClick={ e => handleChange(quantity + 1, e) }
+                    >
+                        <Plus size={ 16 }/>
+                    </Button>
+                    <Input
+                        type="number"
+                        value={ quantity }
+                        className="w-14 text-lg text-center border-0 border-x-2 rounded-none no-spin-buttons"
+                        onClick={ e => preventEventPropagation(e) }
+                        onChange={ e => handleChange(e.target.value, e) }
+                        onBlur={ e => handleBlur(e.target.value) }
+                    />
+                    <Button
+                        variant="outline"
+                        size="icon"
+                        className="border-0 rounded-none"
+                        onClick={ e => handleChange(quantity - 1, e) }
+                    >
+                        <Minus size={ 16 }/>
+                    </Button>
+                </div>
             </div>
-        </div>
-    </ListItem>;
+        </ListItem>
+    </Link>;
 }
