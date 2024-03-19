bb67cbbcae54eb7bbd26bbecda18d5ef6df9b29e
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-03-19 16:37:02 +0000

    Make list item children props optional

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -4,13 +4,13 @@ import { clsx } from "clsx";
 export default function ListItem({ title, className, children, interactive = true }: {
     title?: string;
     className?: string;
-    children: React.ReactNode;
+    children?: React.ReactNode;
     interactive?: boolean;
 }) {
     return <div className={ clsx("rounded-md bg-blue-100 p-2 h-min", { "interactive": interactive }) }>
         { title && <h3 className="text-2xl pb-1">{ title }</h3> }
         <div className={ className }>
-            { children }
+            { children && children }
         </div>
     </div>;
 }
