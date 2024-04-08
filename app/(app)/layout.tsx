3278c52ec0c7239027ab185b5e90a90a3939653b
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-08 18:45:28 +0100

    Change width of the content on large screens

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -3,7 +3,7 @@ import Navigation from "@/components/Navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return <div className="flex flex-col min-h-[calc(100dvh-10px)] md:min-h-[calc(100dvh-20px)] md:flex-row">
-        <div id="content" className="flex flex-col grow md:grow-0 w-full rounded-md lg:w-1/2 lg:mx-auto">
+        <div id="content" className="flex flex-col grow md:grow-0 w-full rounded-md lg:w-8/12 lg:mx-auto">
             { children }
         </div>
         <Navigation/>
