b55d02357c3de8466196ea661779a23671a4a38a
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-08 20:34:00 +0100

    Fix type issue

--- app/(app)/calendar/page.tsx
+++ app/(app)/calendar/page.tsx
@@ -6,9 +6,9 @@ import CalendarItem from "@/components/calendar/CalendarItem";
 
 export default function CalendarPage() {
     const [ date, setDate ] = React.useState<Date>(new Date());
-    const data: { [key: string]: [ { id: number, title: string } ] } = {
         "Wed Apr 10 2024": [ { id: 1, title: "Recycle bins" } ],
         "Mon Apr 08 2024": [ { id: 2, title: "General waste" } ],
+    const data: { [key: string]: { id: string, title: string }[] } = {
     };
 
     return <>
