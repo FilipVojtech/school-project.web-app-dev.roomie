e9514a3303334672615210fbe05813ca7110a3cd
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-08 18:48:59 +0100

    Move metadata from page to layout

--- app/(app)/calendar/layout.tsx
+++ app/(app)/calendar/layout.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import DatePicker from "@/app/(app)/calendar/DatePicker";
 import Header from "@/components/Header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Calendar",
+}
 
 export default function CalendarLayout({ children }: { children: React.ReactNode }) {
     return <>
--- app/(app)/calendar/page.tsx
+++ app/(app)/calendar/page.tsx
@@ -1,9 +1,5 @@
-import { Metadata } from "next";
 import React from "react";
 
-export const metadata: Metadata = {
-    title: "Calendar",
-}
 
 export default function Calendar() {
     const data: { [key: string]: [ { id: number, title: string } ] } = {
