7016ffc4bcbe56c9da7594c246c67c364834134d
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-08 18:46:12 +0100

    Add debug data

--- app/(app)/calendar/page.tsx
+++ app/(app)/calendar/page.tsx
@@ -6,6 +6,10 @@ export const metadata: Metadata = {
 }
 
 export default function Calendar() {
+    const data: { [key: string]: [ { id: number, title: string } ] } = {
+        "Wed Apr 10 2024": [ { id: 1, title: "Recycle bins" } ],
+        "Mon Apr 08 2024": [ { id: 2, title: "General waste" } ],
+    };
 
     return <>
     </>;
