da6a3043ef003ab37c4531bbadb86d8caca4ff1f
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-08 20:32:36 +0100

    Change id type

--- components/calendar/CalendarItem.tsx
+++ components/calendar/CalendarItem.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import ListItem from "@/components/ListItem";
 
-export default function CalendarItem({ id, title }: { id: number; title: string }) {
+export default function CalendarItem({ id, title }: { id: string; title: string }) {
     return <Link href={ `/event/${ id }/edit` }>
         <ListItem title={ title }/>
     </Link>
