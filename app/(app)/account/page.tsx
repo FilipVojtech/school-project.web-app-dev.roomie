7371a7866e18b22d92cd98ee870e0c5f9c45c170
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-11 18:37:41 +0100

    Add sections to settings page

--- app/(app)/account/page.tsx
+++ app/(app)/account/page.tsx
@@ -1,6 +1,11 @@
 import Header from "@/components/Header";
 import { Metadata } from "next";
 import UserSection from "@/components/account/UserSection";
+import GeneralSection from "@/components/account/GeneralSection";
+import AccountSection from "@/components/account/AccountSection";
+import HouseholdManagementSection from "@/components/account/HouseholdManagementSection";
+import LegalSection from "@/components/account/LegalSection";
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
     title: "Account",
@@ -9,6 +14,15 @@ export const metadata: Metadata = {
 export default function Account() {
     return <>
         <Header title="Account"/>
-        <UserSection/>
+        <div className="md:w-1/2 md:mx-auto">
+            <UserSection/>
+            <div className="flex flex-col gap-4 bg-[#E2E8F0] p-2.5 mt-10 rounded-lg">
+                <GeneralSection/>
+                <AccountSection/>
+                <HouseholdManagementSection/>
+                <LegalSection/>
+                <Button variant="destructive">Log out</Button>
+            </div>
+        </div>
     </>;
 };
