b656b39100eebd94f9e1ca51c5a049421f013fa3
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-12 21:20:16 +0100

    Add password validation

--- components/login/RegisterForm.tsx
+++ components/login/RegisterForm.tsx
@@ -20,15 +20,44 @@ const formSchema = z
         lastName: z.string(),
         birthDate: z.date().optional(),
         email: z.string().email(),
-        password: z.string().min(8),
+        password: z.string().min(8, "Password must be at least 8 characters long"),
         passwordRepeat: z.string(),
     })
     .superRefine(({ password, passwordRepeat }, ctx) => {
+        const zIssueObj = {
+            code: z.ZodIssueCode.custom,
+            message: "There was an error",
+            path: [ "password" ],
+        };
+
+        // Password does not contain an uppercase letter
+        if (password.search(new RegExp("[A-Z]")) === -1) {
+            zIssueObj.message = "Password must contain an uppercase letter";
+            ctx.addIssue(zIssueObj);
+        }
+        // Password does not contain a lowercase letter
+        if (password.search(new RegExp("[a-z]")) === -1) {
+            zIssueObj.message = "Password must contain a lowercase letter";
+            ctx.addIssue(zIssueObj);
+        }
+
+        // Password does not contain a digit
+        if (password.search(new RegExp("\\d")) === -1) {
+            zIssueObj.message = "Password must contain a digit";
+            ctx.addIssue(zIssueObj);
+        }
+
+        // Password does not contain a special character
+        if (password.search(new RegExp("[^\\w\\s]")) === -1) {
+            zIssueObj.message = "Password must contain a special character";
+            ctx.addIssue(zIssueObj);
+        }
+
+        // Passwords do not match
         if (password !== passwordRepeat) {
-            ctx.addIssue({
-                code: 'custom',
-                message: "The passwords do not match",
-            })
+            zIssueObj.message = "The passwords do not match";
+            zIssueObj.path = [ "passwordRepeat" ];
+            ctx.addIssue(zIssueObj);
         }
     })
 
@@ -143,8 +172,12 @@ export default function RegisterForm() {
                 ) }
             />
             <Button type="submit" className="w-full">Register</Button>
-            <Button type="button" variant="link" onClick={ () => router.push("/login") }
-                    className="w-full">Login</Button>
+            <Button
+                type="button"
+                variant="link"
+                onClick={ () => router.push("/login") }
+                className="w-full"
+            >Login</Button>
         </form>
     </Form>
 };
