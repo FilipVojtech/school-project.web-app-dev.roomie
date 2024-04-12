9a2fd02cb29f1123f57314c25925802b49a19700
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-12 19:47:27 +0100

    Create a register form

---
+++ components/login/RegisterForm.tsx
@@ -0,0 +1,157 @@
+"use client";
+
+import { z } from "zod";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
+import { Calendar } from "@/components/ui/calendar";
+import { useRouter } from "next/navigation";
+
+const formSchema = z
+    .object({
+        firstName: z.string(),
+        lastName: z.string(),
+        birthDate: z.date().optional(),
+        email: z.string().email(),
+        password: z.string().min(8),
+        passwordRepeat: z.string(),
+    })
+    .superRefine(({ password, passwordRepeat }, ctx) => {
+        if (password !== passwordRepeat) {
+            ctx.addIssue({
+                code: 'custom',
+                message: "The passwords do not match",
+            })
+        }
+    })
+
+export default function RegisterForm() {
+    const router = useRouter();
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+    });
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+    }
+
+    return <Form { ...form }>
+        <form onSubmit={ form.handleSubmit(onSubmit) } className="space-y-2">
+            <FormField
+                control={ form.control }
+                name='firstName'
+                render={ ({ field }) => (
+                    <FormItem>
+                        <FormLabel>First name</FormLabel>
+                        <FormControl>
+                            <Input placeholder="First name" { ...field }/>
+                        </FormControl>
+                        <FormMessage/>
+                    </FormItem>
+                ) }
+            />
+            <FormField
+                control={ form.control }
+                name='lastName'
+                render={ ({ field }) => (
+                    <FormItem>
+                        <FormLabel>Last name</FormLabel>
+                        <FormControl>
+                            <Input placeholder="Last name" { ...field }/>
+                        </FormControl>
+                        <FormMessage/>
+                    </FormItem>
+                ) }
+            />
+            <FormField
+                control={ form.control }
+                name='birthDate'
+                render={ ({ field }) => (
+                    <FormItem>
+                        <FormLabel>Date of birth (optional)</FormLabel>
+                        <Popover>
+                            <PopoverTrigger asChild>
+                                <FormControl>
+                                    <Button
+                                        variant="outline"
+                                        className={ cn(
+                                            "w-full pl-3 text-left font-normal",
+                                            !field.value && "text-muted-foreground"
+                                        ) }
+                                    >
+                                        { field.value ? (
+                                            format(field.value, "PPP")
+                                        ) : (
+                                            <span>Pick a date</span>
+                                        ) }
+                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50"/>
+                                    </Button>
+                                </FormControl>
+                            </PopoverTrigger>
+                            <PopoverContent className="w-auto p-0" align="center">
+                                <Calendar
+                                    mode="single"
+                                    selected={ field.value }
+                                    onSelect={ field.onChange }
+                                    disabled={ (date) =>
+                                        date > new Date() || date < new Date("1900-01-01")
+                                    }
+                                    initialFocus
+                                />
+                            </PopoverContent>
+                        </Popover>
+                        <FormMessage/>
+                    </FormItem>
+                ) }
+            />
+            <FormField
+                control={ form.control }
+                name='email'
+                render={ ({ field }) => (
+                    <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                            <Input placeholder="Email" { ...field }/>
+                        </FormControl>
+                        <FormMessage/>
+                    </FormItem>
+                ) }
+            />
+            <FormField
+                control={ form.control }
+                name='password'
+                render={ ({ field }) => (
+                    <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                            <Input placeholder="Password" { ...field }/>
+                        </FormControl>
+                        <FormMessage/>
+                    </FormItem>
+                ) }
+            />
+            <FormField
+                control={ form.control }
+                name='passwordRepeat'
+                render={ ({ field }) => (
+                    <FormItem>
+                        <FormLabel>Password repeat</FormLabel>
+                        <FormControl>
+                            <Input placeholder="Password repeat" { ...field }/>
+                        </FormControl>
+                        <FormMessage/>
+                    </FormItem>
+                ) }
+            />
+            <Button type="submit" className="w-full">Register</Button>
+            <Button type="button" variant="link" onClick={ () => router.push("/login") } className="w-full">Login</Button>
+        </form>
+    </Form>
+};
