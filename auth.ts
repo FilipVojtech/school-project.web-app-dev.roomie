ea0955473e13618996865d9bd30a77478f37daae
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-15 19:53:47 +0100

    Implement credential provider for auth.js

--- auth.ts
+++ auth.ts
@@ -2,25 +2,19 @@ import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
+import { fetchUser } from "@/app/_actions";
 
 
-export const { auth, signIn, signOut } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
     ...authConfig,
     providers: [ Credentials({
         async authorize(credentials) {
-            const parsedCredentials = z
-                .object({ email: z.string().email(), password: z.string().min(6) })
-                .safeParse(credentials);
+            const { email, password } = credentials as { email: string, password: string };
+            const user = await fetchUser(email);
+            if (!user) return null;
+            const passwordMatch = await bcrypt.compare(password, user.password);
 
-            if (parsedCredentials.success) {
-                const { email, password } = parsedCredentials.data;
-                const user = await getUser(email);
-                if (!user) return null;
-                const passwordMatch = await bcrypt.compare(user.password, password);
-
-                if (passwordMatch) return user;
-            }
-
+            if (passwordMatch) return user;
             return null;
         },
     }) ]
