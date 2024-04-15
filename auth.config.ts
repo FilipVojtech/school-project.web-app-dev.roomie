2100de55fb240dec584ca044afddb03b2b6b422c
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-15 21:27:03 +0100

    Remove commented out code. Sorry, was blinded with rage

--- auth.config.ts
+++ auth.config.ts
@@ -26,53 +26,6 @@ export const authConfig = NextAuth({
             session.user.householdId = householdId;
             return session;
         },
-        // authorized({ auth, request: { nextUrl } }) {
-        //     const restrictedPages = [ "/fridge", "/notes", "/calendar", "/account", "/household", "/create-household" ];
-        //     const isLoggedIn = !!auth?.user;
-        //     const isOnRestrictedPage = ((): boolean => {
-        //         for (const restrictedPage of restrictedPages) {
-        //             if (nextUrl.pathname.startsWith(restrictedPage)) return true;
-        //         }
-        //         return false;
-        //     })()
-        //
-        //     if (isOnRestrictedPage) {
-        //         if (auth?.user?.householdId == null && !nextUrl.pathname.startsWith("/create-household")) {
-        //             return Response.redirect(new URL("/create-household", nextUrl));
-        //         }
-        //         // True - Continue
-        //         // False - Redirect unauthenticated users to login page
-        //         return isLoggedIn;
-        //     }
-        //     // Root page handler
-        //     if (nextUrl.pathname === '/') {
-        //         if (!isLoggedIn) {
-        //             return false;
-        //         } else if (auth.user?.householdId == null) {
-        //             return Response.redirect(new URL('/create-household', nextUrl));
-        //         } else {
-        //             return Response.redirect(new URL('/fridge', nextUrl));
-        //         }
-        //     }
-        //     if (nextUrl.pathname.startsWith("/create-household")) {
-        //         if (!isLoggedIn) {
-        //             return false;
-        //         } else if (auth?.user?.householdId == null) {
-        //             return true
-        //         } else {
-        //             return Response.redirect(new URL("/fridge", nextUrl));
-        //         }
-        //     }
-        //     if (
-        //         isLoggedIn && (
-        //             nextUrl.pathname.startsWith('/login') ||
-        //             nextUrl.pathname.startsWith('/register')
-        //         )
-        //     ) {
-        //         return Response.redirect(new URL('/', nextUrl));
-        //     }
-        //     return true;
-        // },
     },
     providers: [],
 });
