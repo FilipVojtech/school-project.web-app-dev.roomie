4fb5a8a0b2b21971109fc32079f2456b25292ad5
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-16 18:21:03 +0100

    Move contents of auth.config.ts to auth.ts

--- auth.ts
+++ auth.ts
@@ -1,12 +1,44 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+import NextAuth, { type User as AuthUser } from 'next-auth';
 import Credentials from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import { fetchUser } from "@/app/_actions";
+import type { SessionUser, User } from "@/lib/definitions";
 
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-    ...authConfig,
+    pages: {
+        signIn: '/login',
+    },
+    callbacks: {
+        jwt({ token, user }) {
+            if (user) {
+                const { id, email, household_id, first_name, last_name } = user as User;
+                token.id = id;
+                token.householdId = household_id;
+                token.name = `${ first_name } ${ last_name }`;
+                token.initials = first_name[0] + last_name[0];
+                token.email = email;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            const { id, email, name, initials, householdId } = token as SessionUser;
+
+            session.user.id = id;
+            session.user.name = name;
+            session.user.initials = initials;
+            session.user.householdId = householdId;
+
+            const user = await fetchUser(email);
+
+            if (user?.household_id) {
+                session.user.householdId = user.household_id;
+            }
+
+            return session;
+        },
+
+    },
     providers: [ Credentials({
         async authorize(credentials) {
             const { email, password } = credentials as { email: string, password: string };
