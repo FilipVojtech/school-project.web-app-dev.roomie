8cd4552886da9a08a837912c2814cecc923ba740
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-16 18:22:56 +0100

    Center login and registration contents to the center of the page

--- app/(landing)/(login flow)/register/page.tsx
+++ app/(landing)/(login flow)/register/page.tsx
@@ -1,9 +1,9 @@
 import RegisterForm from "@/components/login/RegisterForm";
 
 export default function RegisterPage() {
-    return <div>
+    return <div className="m-auto">
         <h1 className="text-3xl text-center mb-5">Register</h1>
-        <div className="bg-foreground p-1.5 md:p-2 md:w-96 rounded-md m-auto">
+        <div className="bg-foreground p-1.5 md:p-2 md:w-96 rounded-md">
             <RegisterForm/>
         </div>
     </div>
