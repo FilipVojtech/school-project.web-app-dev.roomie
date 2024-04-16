789e5e0986d68048b824cfe36711a8a3911eb135
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-16 18:26:26 +0100

    Remove unused import

--- auth.ts
+++ auth.ts
@@ -1,4 +1,4 @@
-import NextAuth, { type User as AuthUser } from 'next-auth';
+import NextAuth from 'next-auth';
 import Credentials from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import { fetchUser } from "@/app/_actions";
