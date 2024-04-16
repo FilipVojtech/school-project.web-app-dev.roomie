65581deb1329d55d045ae1e9f3f30856bc3b2a9d
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-16 18:30:14 +0100

    Adjust login and register form input spacing

--- components/login/LoginForm.tsx
+++ components/login/LoginForm.tsx
@@ -27,7 +27,7 @@ export default function LoginForm() {
     }
 
     return <Form { ...form }>
-        <form onSubmit={ form.handleSubmit(onSubmit) } className="space-y-2">
+        <form onSubmit={ form.handleSubmit(onSubmit) } className="space-y-5">
             <FormField
                 control={ form.control }
                 name='email'
--- components/login/RegisterForm.tsx
+++ components/login/RegisterForm.tsx
@@ -35,7 +35,7 @@ export default function RegisterForm() {
     }
 
     return <Form { ...form }>
-        <form onSubmit={ form.handleSubmit(onSubmit) } className="space-y-2">
+        <form onSubmit={ form.handleSubmit(onSubmit) } className="space-y-5">
             <FormField
                 control={ form.control }
                 name='firstName'
