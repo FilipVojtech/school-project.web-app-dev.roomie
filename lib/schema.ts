a551c7cc595b65b27544bcd71a9b6b3f865390b3
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-28 17:57:56 +0100

    Add account amendment form schemas

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,5 +1,46 @@
 import { z } from "zod";
 
+const passwordSuperRefine: (arg: {
+    password: string,
+    passwordRepeat: string,
+}, ctx: z.RefinementCtx) => void = ({ password, passwordRepeat }, ctx) => {
+    const zIssueObj = {
+        code: z.ZodIssueCode.custom,
+        message: "There was an error",
+        path: [ "password" ],
+    };
+
+    // Password does not contain an uppercase letter
+    if (password.search(new RegExp("[A-Z]")) === -1) {
+        zIssueObj.message = "Password must contain an uppercase letter";
+        ctx.addIssue(zIssueObj);
+    }
+    // Password does not contain a lowercase letter
+    if (password.search(new RegExp("[a-z]")) === -1) {
+        zIssueObj.message = "Password must contain a lowercase letter";
+        ctx.addIssue(zIssueObj);
+    }
+
+    // Password does not contain a digit
+    if (password.search(new RegExp("\\d")) === -1) {
+        zIssueObj.message = "Password must contain a digit";
+        ctx.addIssue(zIssueObj);
+    }
+
+    // Password does not contain a special character
+    if (password.search(new RegExp("[^\\w\\s]")) === -1) {
+        zIssueObj.message = "Password must contain a special character";
+        ctx.addIssue(zIssueObj);
+    }
+
+    // Passwords do not match
+    if (password !== passwordRepeat) {
+        zIssueObj.message = "The passwords do not match";
+        zIssueObj.path = [ "passwordRepeat" ];
+        ctx.addIssue(zIssueObj);
+    }
+}
+
 export const LoginFormSchema = z.object({
     email: z.string().email("Please enter a valid email address"),
     password: z.string(),
@@ -14,43 +55,7 @@ export const RegisterFormSchema = z
         password: z.string().min(8, "Password must be at least 8 characters long"),
         passwordRepeat: z.string(),
     })
-    .superRefine(({ password, passwordRepeat }, ctx) => {
-        const zIssueObj = {
-            code: z.ZodIssueCode.custom,
-            message: "There was an error",
-            path: [ "password" ],
-        };
-
-        // Password does not contain an uppercase letter
-        if (password.search(new RegExp("[A-Z]")) === -1) {
-            zIssueObj.message = "Password must contain an uppercase letter";
-            ctx.addIssue(zIssueObj);
-        }
-        // Password does not contain a lowercase letter
-        if (password.search(new RegExp("[a-z]")) === -1) {
-            zIssueObj.message = "Password must contain a lowercase letter";
-            ctx.addIssue(zIssueObj);
-        }
-
-        // Password does not contain a digit
-        if (password.search(new RegExp("\\d")) === -1) {
-            zIssueObj.message = "Password must contain a digit";
-            ctx.addIssue(zIssueObj);
-        }
-
-        // Password does not contain a special character
-        if (password.search(new RegExp("[^\\w\\s]")) === -1) {
-            zIssueObj.message = "Password must contain a special character";
-            ctx.addIssue(zIssueObj);
-        }
-
-        // Passwords do not match
-        if (password !== passwordRepeat) {
-            zIssueObj.message = "The passwords do not match";
-            zIssueObj.path = [ "passwordRepeat" ];
-            ctx.addIssue(zIssueObj);
-        }
-    })
+    .superRefine(passwordSuperRefine);
 
 export const CreateHouseholdFormSchema = z
     .object({
@@ -62,3 +67,16 @@ export const ChangeNameFormSchema = z
         firstName: z.string().min(1, "Please enter your first name"),
         lastName: z.string().min(1, "Please enter your last name"),
     })
+
+export const ChangeEmailFormSchema = z
+    .object({
+        email: z.string().email("Please enter a valid email"),
+    })
+
+export const ChangePasswordFormSchema = z
+    .object({
+        currentPassword: z.string().min(0, "Please enter your password."),
+        password: z.string().min(8, "Password must be at least 8 characters long"),
+        passwordRepeat: z.string(),
+    })
+    .superRefine(passwordSuperRefine);
