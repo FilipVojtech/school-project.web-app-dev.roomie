5a4a2c697f2d31fb78a53b87f8db7db5f90f2132
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-28 18:02:29 +0100

    Add options to account section

--- components/account/AccountSection.tsx
+++ components/account/AccountSection.tsx
@@ -1,115 +1,22 @@
-"use client";
-
 import SettingsSection from "@/components/account/SettingsSection";
 import SettingsItem from "@/components/account/SettingsItem";
-import { AtSign, ImageIcon, Lock, LogOut, TextCursorInput, Trash2 } from "lucide-react"
-import {
-    Dialog, DialogClose,
-    DialogContent,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { ChangeNameFormSchema } from "@/lib/schema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useSession } from "next-auth/react"
-import { toast } from "sonner";
-import { changeName } from "@/app/(app)/account/_actions";
+import { ImageIcon } from "lucide-react"
+import ChangeNameOption from "@/components/account/account-section/ChangeNameOption";
+import ChangeEmailOption from "@/components/account/account-section/ChangeEmailOption";
+import { auth } from "@/auth";
+import ChangePasswordOption from "@/components/account/account-section/ChangePasswordOption";
+import LeaveHouseholdOption from "@/components/account/account-section/LeaveHouseholdOption";
+import DeleteAccountOption from "@/components/account/account-section/DeleteAccountOption";
 
-export default function AccountSection() {
+export default async function AccountSection() {
+    const session = await auth();
+
     return <SettingsSection title="Account">
         <SettingsItem title="Update profile picture" icon={ <ImageIcon/> } hidden/>
-        <ChangeNameOption/>
-        <SettingsItem title="Change email" icon={ <AtSign/> }/>
-        <SettingsItem title="Change password" icon={ <Lock/> }/>
-        <SettingsItem title="Leave household" icon={ <LogOut/> } destructive/>
-        <SettingsItem title="Delete account" icon={ <Trash2/> } destructive/>
+        <ChangeNameOption firstName={ session?.user.firstName! } lastName={ session?.user.lastName! }/>
+        <ChangeEmailOption/>
+        <ChangePasswordOption/>
+        <LeaveHouseholdOption/>
+        <DeleteAccountOption/>
     </SettingsSection>;
-}
-
-function ChangeNameOption() {
-    let formDefaults = {
-        firstName: "",
-        lastName: "",
-    };
-    const { update, data: session } = useSession();
-    if (!!session?.user) {
-        formDefaults = {
-            firstName: session.user.firstName,
-            lastName: session.user.lastName,
-        };
-    }
-    const form = useForm<z.infer<typeof ChangeNameFormSchema>>({
-        resolver: zodResolver(ChangeNameFormSchema),
-        defaultValues: formDefaults,
-    });
-
-    async function onSubmit(values: z.infer<typeof ChangeNameFormSchema>) {
-        const result = await changeName(values);
-
-        if (result.success) {
-            toast.success(result.message);
-        } else {
-            toast.error(result.message);
-        }
-    }
-
-    return <>
-        <Dialog>
-            <DialogTrigger asChild>
-                <SettingsItem title="Change name" icon={ <TextCursorInput/> }/>
-            </DialogTrigger>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Change name</DialogTitle>
-                </DialogHeader>
-                <div className="grid gap-4 py-4">
-                    <Form { ...form }>
-                        <form onSubmit={ form.handleSubmit(onSubmit) }>
-                            <FormField
-                                control={ form.control }
-                                name="firstName"
-                                render={ ({ field }) => (
-                                    <FormItem className="grid grid-cols-4 items-center gap-4">
-                                        <FormLabel className="text-right">First name</FormLabel>
-                                        <FormControl>
-                                            <Input placeholder="First name" { ...field } className="col-span-3"/>
-                                        </FormControl>
-                                    </FormItem>
-                                ) }
-                            />
-                            <FormField
-                                control={ form.control }
-                                name="lastName"
-                                render={ ({ field }) => (
-                                    <FormItem className="grid grid-cols-4 items-center gap-4">
-                                        <FormLabel className="text-right">Last name</FormLabel>
-                                        <FormControl>
-                                            <Input placeholder="Last name" { ...field } className="col-span-3"/>
-                                        </FormControl>
-                                    </FormItem>
-                                ) }
-                            />
-                            <DialogFooter className="mt-8 -mb-4">
-                                <DialogClose>
-                                    <Button type="submit" onClick={ () => {
-                                        form.reset({
-                                            firstName: formDefaults.firstName,
-                                            lastName: formDefaults.lastName
-                                        })
-                                    } }>Save changes</Button>
-                                </DialogClose>
-                            </DialogFooter>
-                        </form>
-                    </Form>
-                </div>
-            </DialogContent>
-        </Dialog>
-    </>;
 }
