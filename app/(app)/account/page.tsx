363d584d60f9c5ac49594a528a23f1d5a3d6bf09
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-04-28 17:59:33 +0100

    Remove different states from account page

--- app/(app)/account/page.tsx
+++ app/(app)/account/page.tsx
@@ -6,17 +6,24 @@ import AccountSection from "@/components/account/AccountSection";
 import HouseholdManagementSection from "@/components/account/HouseholdManagementSection";
 import LegalSection from "@/components/account/LegalSection";
 import { Button } from "@/components/ui/button";
-import { signOut } from "@/auth";
+import { auth, signOut } from "@/auth";
 
 export const metadata: Metadata = {
     title: "Account",
 }
 
-export default function Account() {
+export default async function Account() {
+    const session = await auth();
+    if (!session?.user) {
+        return <p>Couldn&apos;t load component</p>
+    }
+
+    const { name, initials, email } = session.user;
+
     return <>
         <Header title="Account"/>
         <div className="md:w-1/2 md:mx-auto">
-            <UserSection/>
+            <UserSection name={ name } initials={ initials } email={ email }/>
             <div className="flex flex-col gap-4 bg-foreground p-2.5 mt-10 rounded-lg">
                 <GeneralSection/>
                 <AccountSection/>
